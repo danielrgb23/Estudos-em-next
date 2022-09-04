deb895773e4b48272f8545f3b1d45cc6a420490c
Daniel <danielrgb2@gmail.com>
2022-09-04 10:27:30 -0300

    Feat:pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head'
 
+import styles from './home.module.scss'
+
 export default function Home() {
   return (
     <>
     <Head>
-      <title>Início | Ig.news</title>
+      <title>Home | Ig.news</title>
     </Head>
-    <h1>Hellou</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
