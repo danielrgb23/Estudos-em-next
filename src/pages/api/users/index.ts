96a868cfb167bae8323c581bf10db1137cb47a3d
Daniel <danielrgb2@gmail.com>
2022-09-05 10:28:50 -0300

    Feat:parametros do usuario

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,5 +1,6 @@
 import {NextApiRequest, NextApiResponse} from 'next'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (request: NextApiRequest, response: NextApiResponse) => {
  const users = [
   {id: 1, name: 'Daniel'},
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
-import {GetServerSideProps} from 'next'
-import Head from 'next/head'
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-import { SubscribeButton } from '../components/SubscribeButton'
-import { stripe } from '../services/stripe'
-
-import styles from './home.module.scss'
+import styles from './home.module.scss';
 
 interface HomeProps {
   product: {
@@ -16,41 +15,39 @@ interface HomeProps {
 export default function Home({ product }: HomeProps) {
   return (
     <>
-    <Head>
-      <title>Home | Ig.news</title>
-    </Head>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>👏Hey, Welcome</span>
+          <span>👏 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
-          <p>
-            Get access to all the publications <br />
+          <p>Get access to all the publications <br />
             <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton priceId={product.priceId}/>
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
-        <img src="/images/avatar.svg" alt="Girl coding"/>
+        <img src="/images/avatar.svg" alt="girl coding." />
       </main>
     </>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1LeJReL4tXzJfFFBG7kVSsTH')
 
   const product = {
     priceId: price.id,
     amount: new Intl.NumberFormat('en-US', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'USD'
     }).format(price.unit_amount / 100),
   }
-  
   return {
     props: {
       product,
     },
-    // revalidate: 60 * 60 * 24//24 horas
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
