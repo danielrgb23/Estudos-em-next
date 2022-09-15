4f6ee8b6f88ba459384c566eb8b980adf410c490
Daniel <danielrgb2@gmail.com>
2022-09-15 14:43:46 -0300

    Feat:Página: post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next/types';
 import { RichText } from 'prismic-dom'
 import Head from 'next/head';
 import Prismic from '@prismicio/client'
+import Link from 'next/link';
 
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss';
@@ -27,11 +28,14 @@ export default function Posts({ posts }: PostsProps) {
    <main className={styles.container}>
     <div className={styles.posts}>
      {posts.map(post => (
-      <a key={post.slug} href="#">
-       <time>{post.updatedAt}</time>
-       <strong>{post.title}</strong>
-       <p>{post.excerpt}</p>
-      </a>
+      // eslint-disable-next-line react/jsx-key
+      <Link  href={`/posts/${post.slug}`}>
+       <a key={post.slug}>
+        <time>{post.updatedAt}</time>
+        <strong>{post.title}</strong>
+        <p>{post.excerpt}</p>
+       </a>
+      </Link>
      ))}
     </div>
    </main>
