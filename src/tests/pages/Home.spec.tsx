3bfda10fe11555dafbe70fde140172db0f0644f7
Daniel <danielrgb2@gmail.com>
2023-01-07 11:57:55 -0300

    feat: Testando pagina de posts

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../../pages';
+import { stripe } from '../../services/stripe';
+import Home, {getStaticProps} from '../../pages';
 
 jest.mock('next-auth/react', () => {
  return {
@@ -19,10 +20,34 @@ jest.mock('next/router', () => {
  }
 })
 
+jest.mock('../../services/stripe')
+
 describe('Home page', () => {
  it('renders correctly', () => {
   render(<Home product={{ priceId: 'fake-price-id', amount: '$10,00' }} />)
 
   expect(screen.getByText('for $10,00 month')).toBeInTheDocument()
+ })
+
+ it('Loads initial data', async () => {
+  const retrieveStripePricesMocked = jest.mocked(stripe.prices.retrieve)
+
+  retrieveStripePricesMocked.mockResolvedValue({
+   id: 'fake-price-id',
+   unit_amount: 1000,
+  } as any)
+
+  const response = await getStaticProps({})
+
+  expect(response).toEqual(
+   expect.objectContaining({
+    props: {
+     product: {
+      priceId: 'fake-price-id',
+      amount: '$10.00'
+     }
+    }
+   })
+  )
  })
 })
